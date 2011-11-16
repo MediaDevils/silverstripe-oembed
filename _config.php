7e296d8f9394cafecde9c6fd096743acdd29cb01
simon <simon@16146551-2698-4681-a6bc-25fd9d950f08>
2011-11-16 22:38:06 +0000

    Sets Flickr oEmbed as autodiscover was not working
    git-svn-id: http://svn.rentbox.co.nz/public/oembed/trunk@51 16146551-2698-4681-a6bc-25fd9d950f08

--- _config.php
+++ _config.php
@@ -2,9 +2,11 @@
 
 oEmbed::add_providers(array(
 	'http://*.youtube.com/watch*' => 'http://www.youtube.com/oembed/',
-	'http://*.flickr.com/*' => true,
+	'http://*.flickr.com/*' => 'http://www.flickr.com/services/oembed/',
 	'http://*.viddler.com/*' => 'http://lab.viddler.com/services/oembed/',
 	'http://*.revision3.com/*' => 'http://revision3.com/api/oembed/',
 	'http://*.hulu.com/watch/*' => 'http://www.hulu.com/api/oembed.json',
 	'http://*.vimeo.com/*' => 'http://www.vimeo.com/api/oembed.json',
 ));
+
+ShortcodeParser::get('default')->register('embed', array('oEmbed', 'handle_shortcode'));
