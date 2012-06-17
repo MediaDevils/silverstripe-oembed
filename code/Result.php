d2ad160330ab10ed7f701aa523ba1e021a6a2ff9
Justin Martin <frozenfire@thefrozenfire.com>
2012-06-16 23:26:11 -0700

    Added docblocks

--- code/Result.php
+++ code/Result.php
@@ -1,5 +1,41 @@
 <?php
+
+/**
+ * SilverStripe oEmbed
+ * 
+ * Provides oEmbed querying and shortcode embedding.
+ * 
+ * @category  module
+ * @package   SilverStripe
+ * @author    Justin Martin <frozenfire@thefrozenfire.com>
+ * @copyright 2012 Justin Martin
+ * @version   2.0
+ * @link      https://github.com/MediaDevils/silverstripe-oembed
+ */
+
+/**
+ * oEmbed Result
+ * 
+ * Handles the processing of URLs into oEmbed result objects.
+ * 
+ * @category  module
+ * @package   SilverStripe
+ * @author    Justin Martin <frozenfire@thefrozenfire.com>
+ * @copyright 2012 Justin Martin
+ * @version   Release: @package_version@
+ * @link      https://github.com/MediaDevils/silverstripe-oembed
+ */
 class oEmbed_Result {
+
+	/**
+	 * Load a URL
+	 * 
+	 * Loads a URL's oEmbed data into an oEmbed result object
+	 * 
+	 * @param string $url URL of oEmbed endpoint to poll
+	 * @return oEmbed_Result_Type oEmbed result object, or false on failure
+	 * @access public 
+	 */
 	public function load($url) {
 		$cache = SS_Cache::factory('oembed');
 		if($result = $cache->load(md5($url))) return unserialize($result);
@@ -22,6 +58,16 @@ class oEmbed_Result {
 		} else return false;
 	}
 	
+	/**
+	 * Loads oEmbed data
+	 * 
+	 * Loads oEmbed data in JSON or XML format into an oEmbed result object
+	 * 
+	 * @param string $data   JSON or XML representation of an oEmbed object
+	 * @param string $format 'json' or 'xml'
+	 * @return oEmbed_Result_Type oEmbed result object, or false on failure
+	 * @access public 
+	 */
 	public function loadData($data, $format) {
 		switch($format) {
 			case 'xml':
@@ -32,10 +78,28 @@ class oEmbed_Result {
 		return false;
 	}
 	
+	/**
+	 * Convert JSON into an oEmbed array
+	 * 
+	 * Convert JSON into an array, representing an oEmbed object
+	 * 
+	 * @param string   $json JSON to convert
+	 * @return array   Decoded array
+	 * @access protected
+	 */
 	protected function fromJSON($json) {
 		return json_decode($json, true);
 	}
 	
+	/**
+	 * Convert XML into an oEmbed array
+	 * 
+	 * Convert XML into an array, representing an oEmbed object
+	 * 
+	 * @param string   $xml XML document to parse
+	 * @return array   Decoded array
+	 * @access protected
+	 */
 	protected function fromXML($xml) {
 		$document = DOMDocument::loadXML($xml);
 		if(!$document) return false;
@@ -71,6 +135,15 @@ class oEmbed_Result {
 		return $oembed;
 	}
 	
+	/**
+	 * Build an oEmbed result
+	 * 
+	 * Build an oEmbed result from an array of properties and values
+	 * 
+	 * @param array  $oembed Array representation of the oEmbed result
+	 * @return oEmbed_Result_Type  oEmbed result object
+	 * @access public
+	 */
 	public function toResult($oembed) {
 		switch($oembed["type"]) {
 			case 'photo':
@@ -84,6 +157,16 @@ class oEmbed_Result {
 		}
 	}
 	
+	/**
+	 * Gets the content type
+	 * 
+	 * Gets the content type for an oEmbed result document from its response
+	 * headers.
+	 * 
+	 * @param array  $headers HTTP response headers from $http_response_header
+	 * @return string The content type
+	 * @access public
+	 */
 	public function getContentType($headers) {
 		foreach($headers as $header) {
 			if(strtolower(substr($header, 0, 13)) == "content-type:") {
@@ -98,24 +181,119 @@ class oEmbed_Result {
 	}
 }
 
+/**
+ * Abstract class for oEmbed result objects
+ * 
+ * Base class for all oEmbed result objects. Represents the common properties.
+ * 
+ * @category  module
+ * @package   SilverStripe
+ * @author	Justin Martin <frozenfire@thefrozenfire.com>
+ * @copyright 2012 Justin Martin
+ * @version   Release: @package_version@
+ * @link	  https://github.com/MediaDevils/silverstripe-oembed
+ */
 abstract class oEmbed_Result_Type extends ViewableData {
+
+	/**
+	 * Result type
+	 * @var string
+	 * @access public 
+	 */
 	public $type;
+
+	/**
+	 * Description for public
+	 * @var unknown
+	 * @access public 
+	 */
 	public $version;
+
+	/**
+	 * Description for public
+	 * @var unknown
+	 * @access public 
+	 */
 	public $title;
+
+	/**
+	 * Description for public
+	 * @var unknown
+	 * @access public 
+	 */
 	public $author_name;
+
+	/**
+	 * Description for public
+	 * @var unknown
+	 * @access public 
+	 */
 	public $author_url;
+
+	/**
+	 * Name of the provider
+	 * @var string
+	 * @access public 
+	 */
 	public $provider_name;
+
+	/**
+	 * URL of the provider
+	 * @var string
+	 * @access public 
+	 */
 	public $provider_url;
+
+	/**
+	 * Seconds to cache result
+	 * @var int
+	 * @access public 
+	 */
 	public $cache_age;
+
+	/**
+	 * URL of a thumbnail
+	 * @var string
+	 * @access public 
+	 */
 	public $thumbnail_url;
+
+	/**
+	 * Width in pixels of the thumbnail
+	 * @var int
+	 * @access public 
+	 */
 	public $thumbnail_width;
+
+	/**
+	 * Height in pixels of the thumbnail
+	 * @var int
+	 * @access public 
+	 */
 	public $thumbnail_height;
 
+	/**
+	 * Constructor for the result object
+	 * 
+	 * @param array  $oembed Array of properties and values
+	 * @return oEmbed_Result_Type  
+	 * @access public
+	 */
 	public function __construct($oembed) {
 		parent::__construct();
 		foreach($oembed as $key => $value) $this->$key = $value;
 	}
 	
+	/**
+	 * Gets the thumbnail for the result
+	 * 
+	 * Gets the thumbnail for the result, as a photo type
+	 * 
+	 * @param unknown $width  Maximum width of the image
+	 * @param unknown $height Maximum height of the image
+	 * @return oEmbed_Result_Photo The thumbnail
+	 * @access public 
+	 */
 	public function Thumbnail($width = null, $height = null) {
 		return new oEmbed_Result_Photo(array(
 			"url" => $this->thumbnail_url,
@@ -125,30 +303,126 @@ abstract class oEmbed_Result_Type extends ViewableData {
 	}
 }
 
+/**
+ * oEmbed Photo Result
+ * 
+ * An oEmbed photo result, and the thumbnail type
+ * 
+ * @category  module
+ * @package   SilverStripe
+ * @author	Justin Martin <frozenfire@thefrozenfire.com>
+ * @copyright 2012 Justin Martin
+ * @version   Release: @package_version@
+ * @link	  https://github.com/MediaDevils/silverstripe-oembed
+ */
 class oEmbed_Result_Photo extends oEmbed_Result_Type {
+
+	/**
+	 * Source URL for the image
+	 * @var string
+	 * @access public 
+	 */
 	public $url;
+
+	/**
+	 * Width of the image in pixels
+	 * @var int
+	 * @access public 
+	 */
 	public $width;
+
+	/**
+	 * Height of the image in pixels
+	 * @var int
+	 * @access public 
+	 */
 	public $height;
 	
+	/**
+	 * HTML for displaying in a template
+	 * 
+	 * @return string HTML for displaying in a template
+	 * @access public
+	 */
 	public function forTemplate() {
 		$url = htmlentities($this->url);
 		return "<img src=\"{$url}\" width=\"{$this->width}\" height=\"{$this->height}\" />";
 	}
 }
 
+/**
+ * oEmbed Video Result
+ * 
+ * An oEmbed video result
+ * 
+ * @category  module
+ * @package   SilverStripe
+ * @author	Justin Martin <frozenfire@thefrozenfire.com>
+ * @copyright 2012 Justin Martin
+ * @version   Release: @package_version@
+ * @link	  https://github.com/MediaDevils/silverstripe-oembed
+ */
 class oEmbed_Result_Video extends oEmbed_Result_Type {
+
+	/**
+	 * HTML for embedding the video
+	 * @var string
+	 * @access public 
+	 */
 	public $html;
+
+	/**
+	 * Width of the video in pixels
+	 * @var int
+	 * @access public 
+	 */
 	public $width;
+
+	/**
+	 * Height of the video in pixels
+	 * @var int
+	 * @access public 
+	 */
 	public $height;
 	
+	/**
+	 * HTML for displaying in a template
+	 * 
+	 * @return string HTML for displaying in a template
+	 * @access public
+	 */
 	public function forTemplate() {
 		return $this->html;
 	}
 }
 
+/**
+ * oEmbed Link Result
+ * 
+ * An oEmbed link result
+ * 
+ * @category  module
+ * @package   SilverStripe
+ * @author	Justin Martin <frozenfire@thefrozenfire.com>
+ * @copyright 2012 Justin Martin
+ * @version   Release: @package_version@
+ * @link	  https://github.com/MediaDevils/silverstripe-oembed
+ */
 class oEmbed_Result_Link extends oEmbed_Result_Type {
+
+	/**
+	 * URL for the link
+	 * @var string
+	 * @access public 
+	 */
 	public $url;
 
+	/**
+	 * HTML for displaying in a template
+	 * 
+	 * @return string HTML for displaying in a template
+	 * @access public
+	 */
 	public function forTemplate() {
 		$url = htmlentities($this->url);
 		$title = htmlspecialchars($this->title);
@@ -156,11 +430,47 @@ class oEmbed_Result_Link extends oEmbed_Result_Type {
 	}
 }
 
+/**
+ * oEmbed Rich Result
+ * 
+ * An oEmbed rich result
+ * 
+ * @category  module
+ * @package   SilverStripe
+ * @author	Justin Martin <frozenfire@thefrozenfire.com>
+ * @copyright 2012 Justin Martin
+ * @version   Release: @package_version@
+ * @link	  https://github.com/MediaDevils/silverstripe-oembed
+ */
 class oEmbed_Result_Rich extends oEmbed_Result_Type {
+
+	/**
+	 * HTML for embedding the rich content
+	 * @var string
+	 * @access public 
+	 */
 	public $html;
+
+	/**
+	 * Width of the rich content in pixels
+	 * @var int
+	 * @access public 
+	 */
 	public $width;
+
+	/**
+	 * Height of the rich content in pixels
+	 * @var int
+	 * @access public 
+	 */
 	public $height;
 	
+	/**
+	 * HTML for displaying in a template
+	 * 
+	 * @return string HTML for displaying in a template
+	 * @access public
+	 */
 	public function forTemplate() {
 		return $this->html;
 	}
--- code/oEmbed.php
+++ code/oEmbed.php
@@ -1,14 +1,74 @@
 <?php
 
+/**
+ * SilverStripe oEmbed
+ * 
+ * Provides oEmbed querying and shortcode embedding.
+ * 
+ * @category  module
+ * @package   SilverStripe
+ * @author    Justin Martin <frozenfire@thefrozenfire.com>
+ * @copyright 2012 Justin Martin
+ * @license   none
+ * @version   2.0
+ * @link      https://github.com/MediaDevils/silverstripe-oembed
+ */
+
+
+/**
+ * oEmbed
+ * 
+ * Provides oEmbed querying and shortcode embedding.
+ * 
+ * @category  module
+ * @package   SilverStripe
+ * @author    Justin Martin <frozenfire@thefrozenfire.com>
+ * @copyright 2012 Justin Martin
+ * @version   Release: @package_version@
+ * @link      https://github.com/MediaDevils/silverstripe-oembed
+ */
 class oEmbed {
+
+	/**
+	 * Array of providers in the format array([<pattern> => <endpoint url>[, ...]])
+	 * @var array	
+	 * @access protected
+	 * @static
+	 */
 	protected static $providers = array();
 	
+	/**
+	 * Whether to fall back to autodiscovery if the provided URL does not match
+	 * @var boolean  
+	 * @access protected
+	 * @static
+	 */
 	protected static $autodiscover_fallback = false;
 	
+	/**
+	 * Set whether to autodiscover
+	 * 
+	 * Set whether to autodiscover when no provider exists for the given URL
+	 * 
+	 * @param boolean $val Whether to autodiscover
+	 * @return void   
+	 * @access public 
+	 * @static
+	 */
 	public static function set_autodiscover_fallback($val) {
 		self::$autodiscover_fallback = (bool)$val;
 	}
 	
+	/**
+	 * Add oEmbed providers
+	 * 
+	 * Add one or more oEmbed providers in the format 
+	 * array([<pattern> => <endpoint url>[, ...]])
+	 * 
+	 * @return void  
+	 * @access public
+	 * @static
+	 */
 	public static function add_providers() {
 		$args = func_get_args();
 		if($args) {
@@ -24,18 +84,57 @@ class oEmbed {
 		}
 	}
 	
+	/**
+	 * Remove a provider by scheme
+	 * 
+	 * Remove a provider from the list of providers, by specifying the matching
+	 * scheme
+	 * 
+	 * @param string $scheme The scheme of the provider to remove
+	 * @return void   
+	 * @access public 
+	 * @static
+	 */
 	public static function remove_provider($scheme) {
 		unset(self::$providers[$scheme]);
 	}
 	
+	/**
+	 * Remove all providers
+	 * 
+	 * @return void  
+	 * @access public
+	 * @static
+	 */
 	public static function empty_providers() {
 		self::$providers = array();
 	}
 	
+	/**
+	 * Get the list of providers
+	 * 
+	 * Get the list of providers in the format
+	 * array([<pattern> => <endpoint url>[, ...]])
+	 * 
+	 * @return array  The array of providers
+	 * @access public
+	 * @static
+	 */
 	public static function get_providers() {
 		return self::$providers;
 	}
 	
+	/**
+	 * Matches a URL to a provider
+	 * 
+	 * Matches a URL to a provider by comparing each provider pattern against
+	 * the URL, returning the endpoint URL of the first matched provider
+	 * 
+	 * @param string $url URL to match
+	 * @return string Provider endpoint, or false if no match
+	 * @access public 
+	 * @static
+	 */
 	public static function match_url($url) {
 		foreach(self::$providers as $scheme=>$endpoint) {
 			if(self::match_scheme($url, $scheme)) {
@@ -45,6 +144,17 @@ class oEmbed {
 		return false;
 	}
 	
+	/**
+	 * Match a URL to a scheme
+	 * 
+	 * Determines whether the provided $url is matched by $scheme
+	 * 
+	 * @param string $url	URL to match
+	 * @param string $scheme Scheme to attempt to match $url with
+	 * @return boolean Whether $url matches $scheme
+	 * @access public 
+	 * @static
+	 */
 	public static function match_scheme($url, $scheme) {
 		$urlInfo = parse_url($url);
 		$schemeInfo = parse_url($scheme);
@@ -68,6 +178,17 @@ class oEmbed {
 		return true;
 	}
 	
+	/**
+	 * Try to autodiscover the endpoint for a URL
+	 * 
+	 * Try to autodiscover the endpoint for a URL by scraping the URL's HTML
+	 * for an endpoint link.
+	 * 
+	 * @param string $url URL to autodiscover an endpoint from
+	 * @return string   Endpoint URL for the specified resource
+	 * @access public 
+	 * @static
+	 */
 	public static function autodiscover_from_url($url) {
 		$service = new RestfulService($url);
 		$body = $service->request();
@@ -88,6 +209,19 @@ class oEmbed {
 		return false;
 	}
 	
+	/**
+	 * Get an oEmbed result object from a URL
+	 * 
+	 * Get an oEmbed result object from a URL if a provider matches, and
+	 * optionally to autodiscover the oEmbed provider if no match is found.
+	 * 
+	 * @param string $url	 URL to get an oEmbed result for
+	 * @param boolean $type	@deprecated
+	 * @param array   $options Custom query parameters to poll the oEmbed service with
+	 * @return oEmbed_Result_Type   An oEmbed result object, or false on failure
+	 * @access public 
+	 * @static
+	 */
 	public static function get_oembed_from_url($url, $type = false, Array $options = array()) {
 		$endpoint = self::match_url($url);
 		$ourl = false;
@@ -118,6 +252,19 @@ class oEmbed {
 		return false;
 	}
 	
+	/**
+	 * Return the embed HTML for a given [embed <url>] tag
+	 * 
+	 * This method handles the shortcode embedding via the WYSIWYG editor
+	 * 
+	 * @param array   $arguments Custom query parameters to poll the oEmbed service with
+	 * @param string  $url	   URL to get the oEmbed result for
+	 * @param unknown $parser	Shortcode parser handle
+	 * @param unknown $shortcode Shortcode being handled
+	 * @return string   HTML markup for the embed
+	 * @access public 
+	 * @static
+	 */
 	public static function handle_shortcode($arguments, $url, $parser, $shortcode) {
 		if(isset($arguments['type'])) {
 			$type = $arguments['type'];
