8791049159a6a7dfa83dee1283a2569224be9c09
Justin Martin <justin@mediadevils.com>
2012-06-25 18:40:27 +0000

    Prelim editor button stuff

--- _config.php
+++ _config.php
@@ -8,7 +8,13 @@ oEmbed::add_providers(array(
 	'http://*.hulu.com/watch/*' => 'http://www.hulu.com/api/oembed.json',
 	'http://*.vimeo.com/*' => 'http://www.vimeo.com/api/oembed.json',
 	'https://twitter.com/*' => 'https://api.twitter.com/1/statuses/oembed.json',
+	'https://*.vimeo.com/*' => 'http://www.vimeo.com/api/oembed.json',
 	'http://twitter.com/*' => 'https://api.twitter.com/1/statuses/oembed.json',
 ));
 
 ShortcodeParser::get('default')->register('embed', array('oEmbed', 'handle_shortcode'));
+
+HTMLEditorConfig::get('cms')->enablePlugins(array(
+	"oembedButton" => "/oembed/oembedButton/oembedButton_src.js"
+));
+HTMLEditorConfig::get('cms')->insertButtonsAfter('image', 'oembedButton');
---
+++ oembedButton/oembedButton_src.js
@@ -0,0 +1,67 @@
+(function() {
+        // Load plugin specific language pack
+        tinymce.PluginManager.requireLangPack('oembedButton');
+
+        tinymce.create('tinymce.plugins.oembedButtonPlugin', {
+                /**
+                 * Initializes the plugin, this will be executed after the plugin has been created.
+                 * This call is done before the editor instance has finished it's initialization so use the onInit event
+                 * of the editor instance to intercept that event.
+                 *
+                 * @param {string} url Absolute URL to where the plugin is located.
+                 */
+                init : function(ed, url) {
+                        // Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceoembedButton');
+                        ed.addCommand('mceoembedButton', function() {
+                                ed.windowManager.open({
+                                        file : url + '/dialog.htm',
+                                        width : 320 + ed.getLang('oembedButton.delta_width', 0),
+                                        height : 120 + ed.getLang('oembedButton.delta_height', 0),
+                                        inline : 1
+                                }, {
+                                        plugin_url : url // Plugin absolute URL
+                                });
+                        });
+
+                        // Register oembedButton button
+                        ed.addButton('oembedButton', {
+                                title : 'oembedButton.desc',
+                                cmd : 'mceoembedButton',
+                                image : url + '/img/oembedButton.gif'
+                        });
+                },
+
+                /**
+                 * Creates control instances based in the incomming name. This method is normally not
+                 * needed since the addButton method of the tinymce.Editor class is a more easy way of adding buttons
+                 * but you sometimes need to create more complex controls like listboxes, split buttons etc then this
+                 * method can be used to create those.
+                 *
+                 * @param {String} n Name of the control to create.
+                 * @param {tinymce.ControlManager} cm Control manager to use inorder to create new control.
+                 * @return {tinymce.ui.Control} New control instance or null if no control was created.
+                 */
+                createControl : function(n, cm) {
+                        return null;
+                },
+
+                /**
+                 * Returns information about the plugin as a name/value array.
+                 * The current keys are longname, author, authorurl, infourl and version.
+                 *
+                 * @return {Object} Name/value array containing information about the plugin.
+                 */
+                getInfo : function() {
+                        return {
+                                longname : 'oembedButton plugin',
+                                author : 'Justin Martin',
+                                authorurl : 'http://github.com/MediaDevils',
+                                infourl : 'https://github.com/MediaDevils/silverstripe-oembed',
+                                version : "1.0"
+                        };
+                }
+        });
+
+        // Register plugin
+        tinymce.PluginManager.add('oembedButton', tinymce.plugins.oembedButtonPlugin);
+})();
